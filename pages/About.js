13a7ea1b14f4ead825c8d090c1ea23745e5f339d
keithghost <keithalphamd@gmail.com>
2025-05-05 11:38:37 +0300

    Create About.js

---
+++ pages/About.js
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About YouTube to MP3 Downloader</h1>
+      <p>
+        This service allows you to easily convert YouTube videos to MP3 audio files.
+        Simply paste a YouTube URL or search for a video, and we'll handle the conversion.
+      </p>
+      <p>
+        We use the Keith Tech API to provide fast and reliable conversions.
+        This is a free service for personal use only.
+      </p>
+      <h2>Disclaimer</h2>
+      <p>
+        Please respect copyright laws. Only download content you have permission to use.
+        We are not responsible for any misuse of this service.
+      </p>
+    </div>
+  );
+};
+
+export default About;
